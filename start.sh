727345848d9c21f6ceb23788f67ac76eccfdfa39
Michael Bao <tcmb139@gmail.com>
2025-09-21 21:23:50 -0400

    docker test x idek

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-celery -A evolutiontransformer.worker.celery_app worker --loglevel=info -c 1 &
+python -m celery -A evolutiontransformer.worker.celery_app worker --loglevel=info -c 1 &
 
-gunicorn evolutiontransformer.api:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:7860
+python -m gunicorn evolutiontransformer.api:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:7860
